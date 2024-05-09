403c4bc64cb2d2ebba040672185b189f5d2e4bc0
IronMan0325 <projectxprojecty5@gmail.com>
2024-05-08 21:06:14 -0700

    Initial commit of the Setu HomePage Service.

---
+++ src/main/java/com/Setu/SetuHomePageService/SetuHomePageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Setu.SetuHomePageService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetuHomePageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetuHomePageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Setu/SetuHomePageService/SetuHomePageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Setu.SetuHomePageService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetuHomePageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
