a21756fcde4eafcd882a07ebb5672c570ef76e8b
Chaitanya Krishna Dirisala <chairana1995@gmail.com>
2024-05-10 23:25:48 -0400

    Code for username proper format validation

--- src/main/java/com/Setu/SetuHomePageService/Authentication/UserLogIn.java
+++ src/main/java/com/Setu/SetuHomePageService/Authentication/UserLogIn.java
@@ -1,5 +1,8 @@
 package com.Setu.SetuHomePageService.Authentication;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserLogIn {
     // Todo: - Get the user details from the function
     public boolean ValidateUserCredentials(String username, String password){
@@ -8,7 +11,10 @@ public class UserLogIn {
 
     private boolean ValidateUserName(String username){
         // Todo: - Check the username is in the proper format.
-        return false;
+        String emailFormat = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(emailFormat);
+        Matcher matcher = pattern.matcher(username);
+        return matcher.matches();
     }
 
     // Todo: - If there is user, then return true for the client.
