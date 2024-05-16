cc6976dd91e8b9a80e68ded1b53b7aff305bcc2e
IronMan0325 <projectxprojecty5@gmail.com>
2024-05-15 21:41:17 -0700

    Adding the TODO information to implement the code.

--- src/main/java/com/Setu/SetuHomePageService/Authentication/CreateNewUser.java
+++ src/main/java/com/Setu/SetuHomePageService/Authentication/CreateNewUser.java
@@ -1,18 +1,51 @@
 package com.Setu.SetuHomePageService.Authentication;
 
-import com.Setu.SetuHomePageService.Utils.NewUserSetu;
+import java.util.HashMap;
 
 public class CreateNewUser {
+
     /**
-     * This function takes the raw input from the user and stores in the DataBase.
+     * Function to take the input data from the creation new user application endpoint.
      */
-    public void AddNewUser(String fullName,
-                                    String email,
-                                    String mobileNumber,
-                                    String password){
-        // Todo: - Before adding the data to the database the user needs to be verified.
-        // Todo: - Email Verification
-        // Todo: - Send the six digit code to the email and verify the user.
-        // Add the new user to the database.
+      public boolean GetNewUserData(HashMap<String, String> newUserData) {
+        if (ValidateEmail() &&
+                ValidateFullName(newUserData.get("FullName")) &&
+                ValidateNumber(newUserData.get("Number")) &&
+                ValidatePassword(newUserData.get("password"))){
+            return true;
+        }
+        return true;
+    }
+
+    /**
+     * Function to valid the length of the password.
+     */
+    private boolean ValidatePassword(String password) {
+        // Todo:- Modify the code as per the conditions.
+        return 6 <= password.length() && password.length() <= 15;
+    }
+
+    /**
+     * Function to verify the input string is mobile number.
+     */
+    private boolean ValidateNumber(String number) {
+        // Todo:- Implement the java code to verify the input string is valid phone number.
+        return false;
+    }
+
+    /**
+     * Function to verify Full Name.
+     */
+    private boolean ValidateFullName(String fullName) {
+        // Todo:- Implement the java code to verify the input string is valid full name.
+        return false;
+    }
+
+    /**
+     * Function to validate the email address.
+     */
+    private boolean ValidateEmail() {
+        // Todo:- Implement the java code to verify the input string is the email address
+        return true;
     }
 }
